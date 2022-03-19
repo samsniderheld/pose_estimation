fixing some normalization bugs

Data_Utils/generate_samples.py
@@ -20,8 +20,8 @@ def get_random_sample(args):
 
     output_paths = sorted(glob.glob(output_data_path),key=natural_keys)
 
-    X = np.empty((1, 53,2))
-    Y = np.empty((1, 53,3))
+    X = np.empty((1, 52,2))
+    Y = np.empty((1, 52,6))
 
     X[0] = np.load(input_paths[rand_idx])
     Y[0] = np.load(output_paths[rand_idx])

Data_Utils/normalize.py
@@ -1,3 +1,4 @@
+from numpy.linalg import norm
 import tensorflow as tf
 import numpy as np
 import random
@@ -11,9 +12,10 @@ from tqdm import tqdm
 
 def normalize_data(args):
 
-    os.makedirs("Normalized_Data")
-    os.makedirs("Normalized_Data/x_data")
-    os.makedirs("Normalized_Data/y_data")
+    if(not os.path.exists("Normalized_Data")):
+      os.makedirs("Normalized_Data")
+      os.makedirs("Normalized_Data/x_data")
+      os.makedirs("Normalized_Data/y_data")
 
     output_path = "Normalized_Data"
     output_path_x = os.path.join(output_path,"x_data")
@@ -31,37 +33,48 @@ def normalize_data(args):
     print("loading data")
     for i, path in tqdm(enumerate(input_paths)):
 
-        X = np.empty((1, 52,2))
-        Y = np.empty((1, 52,6))
+        X = np.empty((52,2),dtype=np.float64)
+        Y = np.empty((52,6),dtype=np.float64)
 
         with open(path) as file:
             csv_reader = csv.reader(file, delimiter=',')
             for j,row in enumerate(csv_reader):
                 for k, val_x in enumerate(row[1:3]):
-                  X[0,j,k] = float(val_x)
+                  X[j,k] = float(val_x)
                 for l, val_y in enumerate(row[1:7]):
-                  Y[0,j,l] = float(val_y)
+                  Y[j,l] = float(val_y)
 
         all_data_x.append(X)
         all_data_y.append(Y)
 
 
     print("calculating mean and std")
-    np_all_data_x = np.array(all_data_x)
-    np_all_data_y = np.array(all_data_y)
+    np_all_data_x = np.array(all_data_x,dtype=np.float64)
+    np_all_data_y = np.array(all_data_y,dtype=np.float64)
 
-    data_x_mean = np.mean(np_all_data_x)
-    data_x_std = np.std(all_data_x)
+    print(np_all_data_x.shape)
+    print(np_all_data_y.shape)
 
-    data_y_mean = np.mean(np_all_data_y)
-    data_y_std = np.std(all_data_y)
+    data_x_mean = np.mean(np_all_data_x,dtype=np.float64,axis=0)
+    data_x_std = np.std(np_all_data_x, dtype=np.float64,axis=0)
+
+    data_y_mean = np.mean(np_all_data_y,dtype=np.float64,axis=0)
+    data_y_std = np.std(np_all_data_y,dtype=np.float64,axis=0)
+
+    # norm_factor_x = np.linalg.norm(np_all_data_x)
+    # norm_factor_y = np.linalg.norm(np_all_data_y)
+
+    
 
 
     print("normalizing and saving data")
-    for i in tqdm(range(np_all_data_x.shape(0))):
+    for i in tqdm(range(0,np_all_data_x.shape[0])):
 
         normalized_x_sample = (np_all_data_x[i]-data_x_mean)/data_x_std
         normalized_y_sample = (np_all_data_y[i]-data_y_mean)/data_y_std
+        
+        # normalized_x_sample = all_data_x[i]/norm_factor_x
+        # normalized_y_sample = all_data_y[i]/norm_factor_y
 
         np.save(os.path.join(output_path_x,f"{i:04d}_x_data"), normalized_x_sample)
         np.save(os.path.join(output_path_y,f"{i:04d}_y_data"), normalized_y_sample)
@@ -69,11 +82,13 @@ def normalize_data(args):
 
     stats_path = os.path.join(args.base_results_dir,"stats.txt")
 
-    with open(stats_path 'W') as f:
+    with open(stats_path, 'w') as f:
         f.write(f"x mean = {data_x_mean}\n")
         f.write(f"x std = {data_x_std}\n")
         f.write(f"y mean = {data_y_mean}\n")
         f.write(f"y std = {data_y_std}\n")
+        # f.write(f"norm factor x = {norm_factor_x}\n")
+        # f.write(f"norm factor y = {norm_factor_y}\n")
 
 
 